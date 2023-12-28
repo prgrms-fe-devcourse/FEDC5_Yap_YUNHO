f24079d1d185e7e420497a10870b1ef1443e35c4
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-12-28 15:11:44 +0900

    [Chore 🚀] TanStack Query ESLint Plugin Setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,14 +6,18 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
     "plugin:prettier/recommended",
+    "plugin:@tanstack/eslint-plugin-query/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "@tanstack/query"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
+    "@tanstack/query/exhaustive-deps": "error",
+    "@tanstack/query/no-rest-destructuring": "warn",
+    "@tanstack/query/stable-query-client": "error",
   },
 }
