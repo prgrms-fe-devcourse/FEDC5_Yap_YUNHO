a1bf6ecdb2ffeb7455d0d9ed67763c1fa95f3773
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-12-30 01:00:08 +0900

    [Design 🎨] GlobalStyles 정리 및 Height값 100% 적용

--- src/GlobalStyles.tsx
+++ src/GlobalStyles.tsx
@@ -1,44 +1,57 @@
 import { createGlobalStyle } from "styled-components"
 
 const GlobalStyles = createGlobalStyle`
-a{
-  text-decoration: none;
-  color: inherit;
-}
 *{
   box-sizing: border-box;
 }
+
 html, body, div, span, h1, h2, h3, h4, h5, h6, p, 
 a, dl, dt, dd, ol, ul, li, form, label, table{
   margin: 0;
   padding: 0;
   border: 0;
-  font-size: 10px;
   vertical-align: baseline;
 }
+
+html{
+  width: 100%;
+  height: 100%;
+  font-size : 62.5%;
+}
+
 body{
+  width: 100%;
+  height: 100%;
   line-height: 1;
   font-family: 'Noto Sans KR', sans-serif;
-  background-color: #F6F9F0;
-  margin-bottom: 100px;
 }
+
+#root {
+  width: 100%;
+  height: 100%;
+}
+
+
 ol, ul{
   list-style: none;
 }
+
 button {
   border: 0;
   background: transparent;
   cursor: pointer;
 }
-html{
-  font-size : 62.5%;
-}
+
 input{
   border : none;
   outline: none;
   background-color: #e9e9e9;
 }
 
+a{
+  text-decoration: none;
+  color: inherit;
+}
 `
 
 export default GlobalStyles
