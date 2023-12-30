449d71535b93fb9a572f13e92526dff37c20ab93
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-12-30 18:51:46 +0900

    [Feat ✏️] POST API 구현 및 파일 분리

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,10 @@
 import axios from "axios"
+import authToken from "../stores/AuthToken"
 
 const { VITE_API_BASE_URL } = import.meta.env
+const API_ERROR_MESSAGE = {
+  POST_DATA_CHECK_ARRAY: "is Array ❌",
+}
 
 export const API = axios.create({
   baseURL: VITE_API_BASE_URL,
@@ -8,14 +12,32 @@ export const API = axios.create({
   timeout: 2000,
 })
 
-export const GET_API = async (path: string, token: string = "") => {
+export const GET_API = async (path: string) => {
   try {
     const res = await API.get(path, {
       headers: {
-        Authorization: token,
+        Authorization: authToken.getToken(),
       },
     })
 
+    return res
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+export const POST_API = async (path: string, data: object) => {
+  try {
+    // 올바른 타입 가드일까?
+    if (data instanceof Array) {
+      return console.error(API_ERROR_MESSAGE.POST_DATA_CHECK_ARRAY)
+    }
+
+    const res = await API.post(path, data, {
+      headers: {
+        Authorization: authToken.getToken(),
+      },
+    })
     return res
   } catch (e) {
     console.error(e)
