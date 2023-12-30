5fa30838ee8262aa28e8e08fe79eff9e7722a88d
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-12-30 23:16:48 +0900

    [Feat ✏️] CategoryBar Props type 지정

--- src/Pages/Home/components/CategoryBar/CategoryBar.Types.ts
+++ src/Pages/Home/components/CategoryBar/CategoryBar.Types.ts
@@ -2,3 +2,10 @@ export interface Category {
   name: string
   id: string
 }
+
+export type OnSelectCategory = (newCategory: Category) => void
+
+export interface CategoryBarProps {
+  selectedCategory: Category
+  onSelected: OnSelectCategory
+}
