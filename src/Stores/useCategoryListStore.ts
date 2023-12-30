701f381586a6d1ca41b522ab1338172e33a41076
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-12-30 23:22:02 +0900

    [Feat ✏️] 작업 편의를 위한 데이터 모델 주석 추가

--- src/Stores/useCategoryListStore.ts
+++ src/Stores/useCategoryListStore.ts
@@ -23,3 +23,26 @@ const useCategoryList = create<UseCategoryList>((set) => ({
 }))
 
 export default useCategoryList
+
+// [
+//   ({
+//     authRequired: false,
+//     posts: [],
+//     _id: "658afcdeef2fa724b416d228",
+//     name: "첫번째",
+//     description: "첫번째",
+//     createdAt: "2023-12-26T16:18:38.952Z",
+//     updatedAt: "2023-12-26T16:18:38.952Z",
+//     __v: 0,
+//   },
+//   {
+//     authRequired: false,
+//     posts: [],
+//     _id: "658afddc9bebfb24f0b5ba8f",
+//     name: "두번째",
+//     description: "658aa44efc3ac71f7467ab27",
+//     createdAt: "2023-12-26T16:22:52.207Z",
+//     updatedAt: "2023-12-26T16:22:52.207Z",
+//     __v: 0,
+//   })
+// ]
