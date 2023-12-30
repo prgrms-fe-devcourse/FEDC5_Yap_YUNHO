94ed61cb28289e88b998c85da71647eaf242da54
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-12-30 22:50:34 +0900

    [Feat ✏️] CategoryBar Component 생성

---
+++ src/Pages/Home/components/CategoryBar/CategoryBar.tsx
@@ -0,0 +1,35 @@
+import * as S from "./CategoryBar.Styles"
+
+const CategoryBar = () => {
+  return <S.CategoryBarLayout></S.CategoryBarLayout>
+}
+
+export default CategoryBar
+
+// [
+//   ({
+//     authRequired: false,
+//     posts: [],
+//     _id: "658afcdeef2fa724b416d228",
+//     name: "첫번째",
+//     description: "첫번째",
+//     createdAt: "2023-12-26T16:18:38.952Z",
+//     updatedAt: "2023-12-26T16:18:38.952Z",
+//     __v: 0,
+//   },
+//   {
+//     authRequired: false,
+//     posts: [],
+//     _id: "658afddc9bebfb24f0b5ba8f",
+//     name: "두번째",
+//     description: "658aa44efc3ac71f7467ab27",
+//     createdAt: "2023-12-26T16:22:52.207Z",
+//     updatedAt: "2023-12-26T16:22:52.207Z",
+//     __v: 0,
+//   })
+// ]
+
+// {
+//   name: "",
+//   id : "",
+// }
