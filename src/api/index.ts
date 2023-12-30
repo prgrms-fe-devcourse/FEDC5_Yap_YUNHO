bfdccc0ab183549c9221614918c90e87b51510a1
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-12-30 17:36:24 +0900

    [Feat ✏️] API_GET 구현

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,12 +4,17 @@ const { VITE_API_BASE_URL } = import.meta.env
 
 export const API = axios.create({
   baseURL: VITE_API_BASE_URL,
-  timeout: 2000, // 요청이 `timeout`보다 오래 걸리면 요청이 중단됩니다.
+  // 요청이 `timeout`보다 오래 걸리면 요청이 중단됩니다.
+  timeout: 2000,
 })
 
-export const GET_API = async (path: string) => {
+export const GET_API = async (path: string, token: string = "") => {
   try {
-    const res = await API.get(path)
+    const res = await API.get(path, {
+      headers: {
+        Authorization: token,
+      },
+    })
 
     return res
   } catch (e) {
