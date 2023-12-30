e20a9203dc525cf940e14db05d4732d812cad5ef
jonguk <sinjuk1@naver.com>
2023-12-30 18:30:05 +0900

    [Feat ✏️] LoginPage 기본적인 Layout 구현

---
+++ src/Pages/Login/Login.styles.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components"
+
+export const MainContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+`
+
+export const HeaderComponent = styled.div`
+  width: 100%;
+  height: 5vh;
+  border-bottom: 0.1rem solid black;
+  font-size: 3rem;
+`
+export const SubContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100%;
+`
+export const ServiceInfoComponent = styled.div`
+  display: flex;
+  width: 50%;
+
+  border-right: 0.1rem solid black;
+  justify-content: center;
+  align-items: center;
+`
+export const LoginComponent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+
+  text-align: center;
+`
--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,5 +1,16 @@
-import React from "react"
+import * as S from "./Login.styles"
+import { Link } from "react-router-dom"
 
 export default function Login() {
-  return <div>Login</div>
+  return (
+    <S.MainContainer>
+      <Link to="/">
+        <S.HeaderComponent>Service Logo</S.HeaderComponent>
+      </Link>
+      <S.SubContainer>
+        <S.ServiceInfoComponent>Service Info Component</S.ServiceInfoComponent>
+        <S.LoginComponent>Login Component</S.LoginComponent>
+      </S.SubContainer>
+    </S.MainContainer>
+  )
 }
