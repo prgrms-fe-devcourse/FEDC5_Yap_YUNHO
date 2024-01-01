f4aacbb04b48e42431d1e1237ac15e45a931021d
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-01 19:41:54 +0900

    [Feat ✏️] 기존 임시 코드 제거 및 useCategoryList hook 연결

--- src/stores/useCategoryListStore.ts
+++ src/stores/useCategoryListStore.ts
@@ -1,48 +1,20 @@
 import { create } from "zustand"
 import { Category } from "@/Pages/Home/components/CategoryBar/CategoryBar.Types"
-import { GET_API } from "@/apis/Api"
 
-export const INITIAL_CATEGORY = { name: "전체", id: "all" }
+export const INITIAL_CATEGORY = {
+  name: "전체",
+  id: "all",
+  description: "장르 불문, 모든 장르를 추천해 드립니다!",
+}
 
 interface UseCategoryList {
   categoryList: Category[]
-  fetchCategoryList: () => void
+  fetchList: (newList: Category[]) => void
 }
 
-const useCategoryList = create<UseCategoryList>((set) => ({
-  categoryList: [INITIAL_CATEGORY],
-  fetchCategoryList: async () => {
-    await GET_API("channels").then((res) => {
-      console.log(res)
-
-      set({ categoryList: [INITIAL_CATEGORY] })
-    })
-  },
+const useCategoryListStore = create<UseCategoryList>((set) => ({
+  categoryList: [],
+  fetchList: (newList) => set({ categoryList: [INITIAL_CATEGORY, ...newList] }),
 }))
 
-// set({ categoryList: [INITIAL_CATEGORY, ...DUMMY_CATEGORY_LIST] }),
-
-export default useCategoryList
-
-// [
-//   ({
-//     authRequired: false,
-//     posts: [],
-//     _id: "658afcdeef2fa724b416d228",
-//     name: "첫번째",
-//     description: "첫번째",
-//     createdAt: "2023-12-26T16:18:38.952Z",
-//     updatedAt: "2023-12-26T16:18:38.952Z",
-//     __v: 0,
-//   },
-//   {
-//     authRequired: false,
-//     posts: [],
-//     _id: "658afddc9bebfb24f0b5ba8f",
-//     name: "두번째",
-//     description: "658aa44efc3ac71f7467ab27",
-//     createdAt: "2023-12-26T16:22:52.207Z",
-//     updatedAt: "2023-12-26T16:22:52.207Z",
-//     __v: 0,
-//   })
-// ]
+export default useCategoryListStore
