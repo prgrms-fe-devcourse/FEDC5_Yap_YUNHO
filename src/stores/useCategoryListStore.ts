7bbfe30bc50313651891ce14c331388413bf373a
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-01 17:54:05 +0900

    [Feat ✏️] api 호출 전 사전 작업

--- src/stores/useCategoryListStore.ts
+++ src/stores/useCategoryListStore.ts
@@ -1,17 +1,8 @@
 import { create } from "zustand"
 import { Category } from "@/Pages/Home/components/CategoryBar/CategoryBar.Types"
+import { GET_API } from "@/apis/Api"
 
-// API 구현 이전까지 테스트 값
-const DUMMY_CATEGORY_LIST: Category[] = [
-  { name: "휴식", id: "11" },
-  { name: "출근길", id: "22" },
-  { name: "퇴근길", id: "33" },
-  { name: "잠들때", id: "44" },
-  { name: "에너지 충전", id: "55" },
-  { name: "로맨스", id: "66" },
-]
-
-const INITIAL_CATEGORY = { name: "전체", id: "all" }
+export const INITIAL_CATEGORY = { name: "전체", id: "all" }
 
 interface UseCategoryList {
   categoryList: Category[]
@@ -20,11 +11,16 @@ interface UseCategoryList {
 
 const useCategoryList = create<UseCategoryList>((set) => ({
   categoryList: [INITIAL_CATEGORY],
+  fetchCategoryList: async () => {
+    await GET_API("channels").then((res) => {
+      console.log(res)
 
-  // 이후 API 와 연결 예정
-  fetchCategoryList: () =>
-    set({ categoryList: [INITIAL_CATEGORY, ...DUMMY_CATEGORY_LIST] }),
+      set({ categoryList: [INITIAL_CATEGORY] })
+    })
+  },
 }))
+
+// set({ categoryList: [INITIAL_CATEGORY, ...DUMMY_CATEGORY_LIST] }),
 
 export default useCategoryList
 
