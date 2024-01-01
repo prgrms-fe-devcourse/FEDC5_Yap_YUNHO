0e7079e815c129a572e7a862113a370db430376e
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-01 11:28:31 +0900

    [Feat ✏️] 스타일 변수 적용 및  Query 구성

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
-import GlobalStyles from "./GlobalStyles.tsx"
+import GlobalStyles from "./styles/GlobalStyles.tsx"
+import { theme } from "./styles/Theme.ts"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ThemeProvider } from "styled-components"
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
