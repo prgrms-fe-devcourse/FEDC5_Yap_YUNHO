cf3c02ed86233f45cce232582c4b70963a13c2ec
J <68155263+jxxxxe@users.noreply.github.com>
2024-01-02 10:50:26 +0900

    [Refactoring️ ⚙️] 프로필 섹션을 컴포넌트로 분리

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,21 +1,12 @@
 import UserPosts from "./Components/UserPosts/UserPosts"
-import UserAction from "./Components/UserProfile/UserAction/UserAction"
-import UserFollowInfo from "./Components/UserProfile/UserFollowInfo"
-import UserNickname from "./Components/UserProfile/UserNickname"
-import UserProfileImage from "./Components/UserProfile/UserProfileImage"
-import * as S from "./Styles/UserProfile.styles"
+import UserProfile from "./components/UserProfile/UserProfile"
 
 const Profile = () => {
   return (
-    <div>
-      <S.UserProfileSection>
-        <UserProfileImage />
-        <UserNickname />
-        <UserAction />
-        <UserFollowInfo />
-      </S.UserProfileSection>
+    <>
+      <UserProfile />
       <UserPosts />
-    </div>
+    </>
   )
 }
 
