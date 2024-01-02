735a55801e8af3b60a5b84c002911a6bf91518d5
J <68155263+jxxxxe@users.noreply.github.com>
2024-01-02 21:44:07 +0900

    [Feat ✏️] 유저 페이지에 navBar 추가

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,11 +1,13 @@
 import UserPosts from "@/pages/Profile/components/UserPosts/UserPosts"
 import UserProfile from "@/pages/Profile/components/UserProfile/UserProfile"
 import { useParams } from "react-router-dom"
+import Navbar from "@/components/Navbar/Navbar"
 
 const Profile = () => {
   const { id } = useParams()
   return (
     <>
+      <Navbar />
       <UserProfile userId={id} />
       <UserPosts />
     </>
