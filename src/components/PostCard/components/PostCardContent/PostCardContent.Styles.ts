b84857556b155966c5015396056946d6748c5b11
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-02 13:43:54 +0900

    [Design 🎨] 폰트 관련 개선

--- src/components/PostCard/components/PostCardContent/PostCardContent.Styles.ts
+++ src/components/PostCard/components/PostCardContent/PostCardContent.Styles.ts
@@ -5,22 +5,30 @@ export const PostCardContentLayout = styled.div`
   height: 18%;
   margin-top: 3%;
   margin-left: 15%;
-  padding: 10%;
+  padding: 2%;
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  font-size: 1.4rem;
-  text-align: center;
-
-  border-radius: 8px;
-
+  border-radius: ${({ theme }) => theme.radius.size8};
   background-color: ${({ theme }) => theme.colors.white};
+
+  font-size: ${({ theme }) => theme.fontSizes.medium};
+  text-align: center;
 `
 
 export const PostCardContentText = styled.h4<{ $textPercentage?: string }>`
+  height: 100%;
+  width: 100%;
+
+  display: flex;
+
+  text-align: center;
+  line-height: 1.4;
   font-size: ${({ $textPercentage }) =>
     $textPercentage ? $textPercentage : "100%"};
-  border: 1px solid red;
+
+  overflow-y: scroll;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `
