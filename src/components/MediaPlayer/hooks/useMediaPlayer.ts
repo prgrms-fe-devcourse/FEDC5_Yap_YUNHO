1273382b3b5c48f4f46f95082fd31177d13c9c5f
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-02 20:51:50 +0900

    [Feat ✏️] useMediaPlayer hook 구현

---
+++ src/components/MediaPlayer/hooks/useMediaPlayer.ts
@@ -0,0 +1,78 @@
+import useMediaPlayerStore from "@/components/MediaPlayer/store/useMediaPlayerStore"
+import { useRef } from "react"
+import ReactPlayer from "react-player"
+import {
+  ChangePlayer,
+  ChangeUrl,
+  TogglePlayer,
+} from "../store/useMediaPlayerStore.Types"
+import {
+  MEDIA_PLAYER_EMPTY_URL_KEYWORD,
+  PROGRESS_BAR_RANGE_PERCENTAGE,
+} from "../constants/MediaPlayer"
+
+const useMediaPlayer = () => {
+  const playerRef = useRef<ReactPlayer>(null)
+  const {
+    playUrl,
+    isMute,
+    isPlaying,
+    playRange,
+    volume,
+    togglePlayer,
+    changeRange,
+    changeUrl,
+  } = useMediaPlayerStore()
+
+  const onClickPlayer: TogglePlayer = (toggleType) => {
+    togglePlayer(toggleType)
+  }
+
+  const onChangeRange: ChangePlayer = ({ type, percentage }) => {
+    const { current } = playerRef
+
+    if (
+      !current &&
+      percentage < PROGRESS_BAR_RANGE_PERCENTAGE.MIN &&
+      percentage > PROGRESS_BAR_RANGE_PERCENTAGE.MAX
+    ) {
+      return
+    }
+
+    if (!(current instanceof ReactPlayer)) {
+      return
+    }
+
+    changeRange({ type, percentage })
+
+    if (type === "play") {
+      current.seekTo(percentage)
+    }
+  }
+
+  const onChangeUrl: ChangeUrl = (url) => {
+    if (url === MEDIA_PLAYER_EMPTY_URL_KEYWORD) {
+      changeUrl("")
+      return
+    }
+
+    const isCanPlay = ReactPlayer.canPlay(url)
+    if (isCanPlay) {
+      changeUrl(url)
+    }
+  }
+
+  return {
+    playUrl,
+    isPlaying,
+    isMute,
+    playerRef,
+    playRange,
+    volume,
+    onClickPlayer,
+    onChangeRange,
+    onChangeUrl,
+  }
+}
+
+export default useMediaPlayer
