b15e4d5f37444f1324f17ef67763dab789b83233
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-03 11:36:48 +0900

    [Feat ✏️] slider 연결

--- src/components/PostCard/components/MediaPlayer/MediaPlayer.tsx
+++ src/components/PostCard/components/MediaPlayer/MediaPlayer.tsx
@@ -1,48 +1,83 @@
-import { ChangeEvent, MouseEvent } from "react"
+import { MouseEvent, useCallback } from "react"
 import * as S from "./MediaPlayer.Styles"
 import useMediaPlayer from "./hooks/useMediaPlayer"
 import { MediaPlayerProps } from "./MediaPlayer.Types"
+import MediaPlayerSlider from "./components/MediaPlayerPlaySlider/MediaPlayerSlider"
+import ReactPlayer from "react-player"
+import { ChangePlayer } from "./store/useMediaPlayerStore.Types"
 
 const MediaPlayer = ({ isBlock }: MediaPlayerProps): React.ReactNode => {
-  const { onClickPlayer, onChangeRange } = useMediaPlayer()
+  const {
+    playerRef,
+    isPlaying,
+    isMute,
+    playUrl,
+    volume,
+    onClickPlayer,
+    onChangeRange,
+  } = useMediaPlayer()
 
-  const handleClickPlayer = ({ target }: MouseEvent) => {
-    if (isBlock) {
-      return
-    }
+  const handleClickPlayer = useCallback(
+    ({ target }: MouseEvent) => {
+      if (isBlock) {
+        return
+      }
 
-    if (!(target instanceof HTMLElement)) {
-      return
-    }
+      if (!(target instanceof HTMLElement)) {
+        return
+      }
 
-    const { clickType } = target.dataset
-    if (clickType === "play" || clickType === "mute") {
-      onClickPlayer({ type: clickType })
-    }
-  }
+      const { clickType } = target.dataset
+      if (clickType === "play" || clickType === "mute") {
+        onClickPlayer({ type: clickType })
+      }
+    },
+    [isBlock, onClickPlayer],
+  )
 
-  const handleChangePlayer = ({ target }: ChangeEvent) => {
-    if (isBlock) {
-      return
-    }
+  const handleChangePlayer: ChangePlayer = useCallback(
+    (newRange) => {
+      if (isBlock) {
+        return
+      }
 
-    if (!(target instanceof HTMLInputElement)) {
-      return
-    }
+      onChangeRange(newRange)
+    },
+    [isBlock, onChangeRange],
+  )
 
-    const { value, dataset } = target
-    const { changeType } = dataset
-    const convertedValue = parseFloat(value)
-    if (!changeType) {
-      return
-    }
-
-    if (changeType === "volume" || changeType === "play") {
-      onChangeRange({ type: changeType, percentage: convertedValue })
-    }
-  }
-
-  return <S.MediaPlayerLayout></S.MediaPlayerLayout>
+  return (
+    <S.MediaPlayerLayout>
+      <S.MediaPlayerHidden>
+        <ReactPlayer
+          ref={playerRef}
+          playing={isBlock ? false : isPlaying}
+          muted={isBlock ? true : isMute}
+          volume={volume}
+          url={"https://www.youtube.com/watch?v=2gliGzb2_1I&t=2703s"}
+          config={{
+            youtube: {
+              playerVars: {
+                origin: window.location.origin,
+              },
+            },
+          }}
+        />
+      </S.MediaPlayerHidden>
+      <MediaPlayerSlider
+        changeType="play"
+        isBlock={false}
+        initialValue={0.2}
+        onChange={handleChangePlayer}
+      />
+      <button
+        onClick={handleClickPlayer}
+        data-click-type="play"
+      >
+        asdasdas
+      </button>
+    </S.MediaPlayerLayout>
+  )
 }
 
 export default MediaPlayer
