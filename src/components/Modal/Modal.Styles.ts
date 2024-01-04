e26195b4001e7389c490ab46408a319786516ae6
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-04 13:26:09 +0900

    [Feat ✏️] Modal 외형 구현

--- src/components/Modal/Modal.Styles.ts
+++ src/components/Modal/Modal.Styles.ts
@@ -4,9 +4,57 @@ export const ModalBackground = styled.section`
   width: 100%;
   height: 100%;
 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   position: absolute;
   top: 0;
 
-  z-index: 5;
+  z-index: ${({ theme }) => theme.zIndex.modalBackground};
   background-color: rgba(0, 0, 0, 0.5);
+`
+
+export const ModalSection = styled.section`
+  width: 106rem;
+  height: 80rem;
+  padding: 0 3rem 3rem 3rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  background-color: ${({ theme }) => theme.colors.sub};
+  border-radius: ${({ theme }) => theme.radius.size12};
+  box-shadow: 0 0 4px 4px rgba(0, 0, 0, 0.5);
+`
+
+export const ModalTop = styled.div`
+  width: 100%;
+  height: 5rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  svg {
+    font-size: 3.2rem;
+    font-weight: 900;
+    color: ${({ theme }) => theme.colors.point};
+
+    cursor: pointer;
+    transition: all 0.3s;
+
+    &:hover {
+      transform: scale(1.4);
+    }
+  }
+`
+
+export const ModalContent = styled.div`
+  width: 100%;
+  flex-grow: 1;
+
+  background-color: orange;
 `
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,7 +1,17 @@
 import * as S from "./Modal.Styles"
+import CloseIcon from "@mui/icons-material/Close"
 
 const Modal = () => {
-  return <S.ModalBackground></S.ModalBackground>
+  return (
+    <S.ModalBackground>
+      <S.ModalSection>
+        <S.ModalTop>
+          <CloseIcon />
+        </S.ModalTop>
+        <S.ModalContent></S.ModalContent>
+      </S.ModalSection>
+    </S.ModalBackground>
+  )
 }
 
 export default Modal
