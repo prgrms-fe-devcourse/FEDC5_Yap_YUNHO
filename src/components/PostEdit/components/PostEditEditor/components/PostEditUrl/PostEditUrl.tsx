b6078dfb0908c7bf05240705ccfd35947941d315
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-05 18:43:42 +0900

    [Feat ✏️] confirm 모달을 통한 Url 전체 삭제

--- src/components/PostEdit/components/PostEditEditor/components/PostEditUrl/PostEditUrl.tsx
+++ src/components/PostEdit/components/PostEditEditor/components/PostEditUrl/PostEditUrl.tsx
@@ -5,6 +5,7 @@ import SmallModal from "@/components/Modal/SmallModal"
 import PostEditPrompt from "./components/PostEditPrompt"
 import DeleteIcon from "@mui/icons-material/Delete"
 import { POST_EDIT_EDITOR_PLACEHOLDER } from "@/components/PostEdit/constants/PostEdit.Constants"
+import ModalConfirm from "@/components/ModalConfirm/ModalConfirm"
 
 interface PostEditUrlProps {
   urlPath: string
@@ -12,7 +13,16 @@ interface PostEditUrlProps {
 }
 
 const PostEditUrl = ({ urlPath, onEdit }: PostEditUrlProps) => {
-  const { isModalToggle, showModal, closeModal } = useModal()
+  const {
+    isModalToggle: isPromptToggle,
+    closeModal: closePrompt,
+    showModal: showPrompt,
+  } = useModal()
+  const {
+    isModalToggle: isConfirmToggle,
+    closeModal: closeConfirm,
+    showModal: showConfirm,
+  } = useModal()
 
   const handleEditUrl = (newUrl: string) => {
     onEdit({
@@ -21,31 +31,49 @@ const PostEditUrl = ({ urlPath, onEdit }: PostEditUrlProps) => {
     })
   }
 
+  const handleRemoveUrl = (response: boolean) => {
+    console.log(response)
+    if (response) {
+      onEdit({
+        type: "mediaUrl",
+        value: "",
+      })
+    }
+
+    closeConfirm()
+  }
+
   return (
     <>
       <S.PostEditUrlLayout>
         <S.PostEditUrlTitle>
           <S.PostEitUrlTitleText> 영상 URL</S.PostEitUrlTitleText>
-          <DeleteIcon />
+          <DeleteIcon onClick={showConfirm} />
         </S.PostEditUrlTitle>
         <S.PostEditUrlInput
           readOnly
           value={urlPath}
           placeholder={POST_EDIT_EDITOR_PLACEHOLDER.URL_INPUT}
-          onClick={() => showModal()}
+          onClick={() => showPrompt()}
         />
       </S.PostEditUrlLayout>
 
       <SmallModal
-        isShow={isModalToggle}
+        isShow={isPromptToggle}
         $height={30}
       >
         <PostEditPrompt
-          onClose={closeModal}
+          onClose={closePrompt}
           onEdit={handleEditUrl}
           url={urlPath}
         />
       </SmallModal>
+
+      <ModalConfirm
+        isShow={isConfirmToggle}
+        onClose={handleRemoveUrl}
+        message="URL을 초기화 하시겠습니까?"
+      />
     </>
   )
 }
