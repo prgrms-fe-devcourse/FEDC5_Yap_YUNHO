bfd7326824f01a810f67ab79cc52d26c604d3ddc
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-05 14:58:16 +0900

    [Feat ✏️] PostEditInput 구현

--- src/components/PostEdit/PostEdit.tsx
+++ src/components/PostEdit/PostEdit.tsx
@@ -19,6 +19,7 @@ const PostEdit = () => {
     setEditPost(newEditPost)
   }
 
+  console.log(editPost)
   return (
     <S.PostEditLayout>
       <S.PostEditContainer>
@@ -26,7 +27,10 @@ const PostEdit = () => {
       </S.PostEditContainer>
       <S.PostEditBoundary />
       <S.PostEditContainer>
-        <PostEditEditor onEdit={handleEditPost} />
+        <PostEditEditor
+          onEdit={handleEditPost}
+          postData={editPost}
+        />
       </S.PostEditContainer>
     </S.PostEditLayout>
   )
--- src/components/PostEdit/components/PostEditEditor/PostEditEditor.Styles.ts
+++ src/components/PostEdit/components/PostEditEditor/PostEditEditor.Styles.ts
@@ -4,6 +4,10 @@ export const PostEditEditorLayout = styled.div`
   width: 46rem;
   height: 63rem;
 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   background-color: ${({ theme }) => theme.colors.sub_alt};
   border-radius: ${({ theme }) => theme.radius.size12};
 `
