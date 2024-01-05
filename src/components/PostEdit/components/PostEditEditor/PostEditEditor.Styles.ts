24dc132d69cb30df55b22a6d2d218fc0c3a17569
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-05 09:40:11 +0900

    [Feat ✏️] PostEditEditor Component 구현

---
+++ src/components/PostEdit/components/PostEditEditor/PostEditEditor.Styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+
+export const PostEditEditorLayout = styled.div`
+  width: 42rem;
+  height: 63rem;
+
+  background-color: ${({ theme }) => theme.colors.sub_alt};
+`
