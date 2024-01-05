096d6bbfa9cdc64858eafc59ebdb8afacdfc3423
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-05 09:39:05 +0900

    [Feat ✏️] PostEditViewer Component 제작

---
+++ src/components/PostEdit/components/PostEditViewer/PostEditViewer.Styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+
+export const PostEditViewerLayout = styled.div`
+  width: 42rem;
+  height: 63rem;
+
+  background-color: ${({ theme }) => theme.colors.sub_alt};
+`
