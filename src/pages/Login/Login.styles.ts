e637af7cc744a86e66b14c2b213beeb7d2df0c16
jonguk <sinjuk1@naver.com>
2024-01-06 22:35:12 +0900

    [Design 🎨] 최소 높이 추가

--- src/pages/Login/Login.styles.ts
+++ src/pages/Login/Login.styles.ts
@@ -11,8 +11,8 @@ export const LoginFormContainer = styled.div`
   flex-direction: row;
   color: ${({ theme }) => theme.colors.white};
   width: 100%;
-
   height: 100%;
+  min-height: 50rem;
   @media screen and (max-width: 50rem) {
     flex-direction: column;
     height: 50rem;
