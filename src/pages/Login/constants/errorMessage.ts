18be096cd7e8a8272ae0cb459e24be0a98871a2e
jonguk <sinjuk1@naver.com>
2024-01-06 16:49:51 +0900

    [Refactoring ⚙️] password validation 시 magic number대신 상수 쓰도록 수정

--- src/pages/Login/constants/errorMessage.ts
+++ src/pages/Login/constants/errorMessage.ts
@@ -1,2 +1,3 @@
+import { MIN_LENGTH } from "./passwordValidation"
 export const EMAIL_ERROR_MESSAGE = "유효한 이메일을 입력해주세요."
-export const PASSWORD_ERROR_MESSAGE = "비밀번호는 최소 8자 이상이어야 합니다."
+export const PASSWORD_ERROR_MESSAGE = `비밀번호는 최소 ${MIN_LENGTH}자 이상이어야 합니다.`
