d569be984f3931e292833773f4e95f1fc8841be3
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-07 20:42:00 +0900

    [Change 🚜] PostEdit의 타입 재지정

--- src/components/PostEdit/PostEdit.Types.ts
+++ src/components/PostEdit/PostEdit.Types.ts
@@ -2,3 +2,10 @@ export type HandleEditPost = (param: {
   type: "thumbnail" | "mediaUrl" | "content" | "category"
   value: string
 }) => void
+
+export interface EditPostState {
+  thumbnail: string
+  mediaUrl: string
+  content: string
+  category: string
+}
--- src/components/PostEdit/constants/PostEdit.Constants.ts
+++ src/components/PostEdit/constants/PostEdit.Constants.ts
@@ -1,14 +1,15 @@
-import { PostContent } from "@/types"
+import { EditPostState } from "../PostEdit.Types"
 
 export const NOT_FOUND_IMAGE = {
   URL: "/src/assets/not_found_img.jpg",
   CHECK_KEY: "not_found_Image",
 }
 
-export const POST_EDIT_INITIAL_EDIT_POST: PostContent = {
+export const POST_EDIT_INITIAL_EDIT_POST: EditPostState = {
   thumbnail: NOT_FOUND_IMAGE.CHECK_KEY,
   content: "",
   mediaUrl: "",
+  category: "",
 }
 
 export const POST_EDIT_EDITOR_PLACEHOLDER = {
