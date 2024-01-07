a4823aebb824423cb66e232545f9476788d2a13e
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-07 18:51:21 +0900

    [Feat ✏️] not_found_image 추가

--- src/components/PostEdit/constants/PostEdit.Constants.ts
+++ src/components/PostEdit/constants/PostEdit.Constants.ts
@@ -1,7 +1,12 @@
 import { PostContent } from "@/types"
 
+export const NOT_FOUND_IMAGE = {
+  URL: "/src/assets/not_found_img.jpg",
+  CHECK_KEY: "not_found_Image",
+}
+
 export const POST_EDIT_INITIAL_EDIT_POST: PostContent = {
-  thumbnail: "",
+  thumbnail: NOT_FOUND_IMAGE.CHECK_KEY,
   content: "",
   mediaUrl: "",
 }
