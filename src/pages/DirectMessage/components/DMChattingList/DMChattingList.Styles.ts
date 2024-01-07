e13fc7255f3148b460835928af5367fcc5f442a6
최훈오 <gnsdh86166@gmail.com>
2024-01-07 16:21:37 +0900

    [Feat ✏️] 라우팅에 따른 메시지 출력

--- src/pages/DirectMessage/components/DMChattingList/DMChattingList.Styles.ts
+++ src/pages/DirectMessage/components/DMChattingList/DMChattingList.Styles.ts
@@ -5,4 +5,12 @@ export const DMChattingListLayout = styled.div`
   height: 100%;
   background-color: ${({ theme }) => theme.colors.sub_alt};
   border-radius: ${({ theme }) => theme.radius.size20};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem;
+`
+
+export const DMChattingListMessageItem = styled.div`
+  font-size: ${({ theme }) => theme.fontSizes.large};
 `
--- src/pages/DirectMessage/components/DMChattingList/DMChattingList.tsx
+++ src/pages/DirectMessage/components/DMChattingList/DMChattingList.tsx
@@ -1,8 +1,44 @@
 import { useParams } from "react-router-dom"
 import * as S from "./DMChattingList.Styles"
+import { AUTH_API } from "@/apis/Api"
+import { ReactNode, useCallback, useEffect, useState } from "react"
+import { Conversation } from "@/types"
 const DMChattingList = () => {
   const { id } = useParams()
-  console.log(id)
-  return <S.DMChattingListLayout></S.DMChattingListLayout>
+  const [selectedMessageList, setSelectedMessgaeList] = useState([])
+
+  const fetchMessageList = useCallback(async () => {
+    return await AUTH_API.get(`/messages?userId=${id}`)
+      .then((res) => setSelectedMessgaeList(res.data))
+      .catch((error) => {
+        console.log(error, "메시지 검색 오류")
+      })
+  }, [id])
+
+  useEffect(() => {
+    // 기본 페이지에서는 메시지 받아오지 않기
+    if (!id) {
+      return
+    }
+    fetchMessageList()
+  }, [id, fetchMessageList])
+
+  return (
+    <S.DMChattingListLayout>
+      {id && (
+        <S.DMChattingListMessageItem>
+          {selectedMessageList.map(
+            (list: Conversation): ReactNode => (
+              <p>
+                {list.message}
+                <br />
+                <br />
+              </p>
+            ),
+          )}
+        </S.DMChattingListMessageItem>
+      )}
+    </S.DMChattingListLayout>
+  )
 }
 export default DMChattingList
