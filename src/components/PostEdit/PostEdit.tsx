bd0e2d74b41ef43f57e91723b48d2ab579d4c768
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-07 20:43:44 +0900

    [Feat ✏️] Media Thumbnail 등록

--- src/components/PostEdit/PostEdit.tsx
+++ src/components/PostEdit/PostEdit.tsx
@@ -2,16 +2,26 @@ import { useState } from "react"
 import * as S from "./PostEdit.Styles"
 import PostEditEditor from "./components/PostEditEditor/PostEditEditor"
 import PostEditViewer from "./components/PostEditViewer/PostEditViewer"
-import { PostContent } from "@/types"
 import { POST_EDIT_INITIAL_EDIT_POST } from "./constants/PostEdit.Constants"
-import { HandleEditPost } from "./PostEdit.Types"
+import { EditPostState, HandleEditPost } from "./PostEdit.Types"
+import getThumbnailByUrl from "@/util/getThumbnailByUrl"
 
 const PostEdit = () => {
-  const [editPost, setEditPost] = useState<PostContent>(
+  const [editPost, setEditPost] = useState<EditPostState>(
     POST_EDIT_INITIAL_EDIT_POST,
   )
 
   const handleEditPost: HandleEditPost = ({ type, value }) => {
+    if (type === "mediaUrl") {
+      const newEditPost = {
+        ...editPost,
+        mediaUrl: value,
+        thumbnail: getThumbnailByUrl(value),
+      }
+      setEditPost(newEditPost)
+      return
+    }
+
     const newEditPost = {
       ...editPost,
       [type]: value,
