8377f9607fe2e80a4110dce44b36a2d2f848b5b9
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-07 21:49:25 +0900

    [Feat ✏️] PostEditCategory List layout

--- src/components/PostEdit/components/PostEditEditor/components/PostEditCategory/PostEditCategory.Styles.ts
+++ src/components/PostEdit/components/PostEditEditor/components/PostEditCategory/PostEditCategory.Styles.ts
@@ -14,3 +14,34 @@ export const PostEditCategoryTitle = styled.h6`
   color: ${({ theme }) => theme.colors.main};
   font-size: ${({ theme }) => theme.fontSizes.large};
 `
+
+export const PostEditCategoryList = styled.ul`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 1rem 1rem;
+
+  font-size: ${({ theme }) => theme.fontSizes.large};
+`
+
+export const PostEditCategoryItem = styled.li<{ $isSelect: boolean }>`
+  padding: 1rem 1.2rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-color: ${({ theme, $isSelect }) =>
+    $isSelect ? theme.colors.point_alt : theme.colors.point};
+  border-radius: ${({ theme }) => theme.radius.size8};
+
+  white-space: nowrap;
+
+  user-select: none;
+  cursor: pointer;
+
+  box-shadow: 0 0 2px 2px rgba(0, 0, 0, 0.5);
+`
--- src/components/PostEdit/components/PostEditEditor/components/PostEditCategory/PostEditCategory.tsx
+++ src/components/PostEdit/components/PostEditEditor/components/PostEditCategory/PostEditCategory.tsx
@@ -1,9 +1,23 @@
+import useCategoryList from "@/hooks/useCategoryList"
 import * as S from "./PostEditCategory.Styles"
 
 const PostEditCategory = () => {
+  const categoryList = useCategoryList()
   return (
     <S.PostEditCategoryLayout>
       <S.PostEditCategoryTitle>카테고리</S.PostEditCategoryTitle>
+      <S.PostEditCategoryList>
+        {categoryList &&
+          categoryList.map((category) => {
+            if (category.id === "all") {
+              return
+            }
+
+            return (
+              <S.PostEditCategoryItem>{category.name}</S.PostEditCategoryItem>
+            )
+          })}
+      </S.PostEditCategoryList>
     </S.PostEditCategoryLayout>
   )
 }
