9eec5d03210f910acc82c8bdb7331e0fd816ecca
J <68155263+jxxxxe@users.noreply.github.com>
2024-01-07 20:37:27 +0900

    [Design 🎨] Search 모달 전체 레이아웃 구현

--- src/components/Search/Search.Styles.ts
+++ src/components/Search/Search.Styles.ts
@@ -3,4 +3,11 @@ import styled from "styled-components"
 export const SearchLayout = styled.div`
   width: 100%;
   height: 100%;
+  padding: 5%;
+
+  display: flex;
+  flex-direction: column;
+
+  background-color: ${({ theme }) => theme.colors.sub_alt};
+  border-radius: ${({ theme }) => theme.radius.size12};
 `
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,7 +1,14 @@
 import * as S from "./Search.Styles"
+import SearchInput from "./components/SearchInput/SearchInput"
+import { SearchResultLayout } from "./components/SearchResult/SearchResult.Styles"
 
 const Search = () => {
-  return <S.SearchLayout></S.SearchLayout>
+  return (
+    <S.SearchLayout>
+      <SearchInput />
+      <SearchResultLayout />
+    </S.SearchLayout>
+  )
 }
 
 export default Search
