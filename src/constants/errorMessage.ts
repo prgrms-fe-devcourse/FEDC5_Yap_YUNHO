a9866abccc5b1ba5e34867ce7b19ad6e2d5aa6d7
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-08 13:38:46 +0900

    [Feat ✏️] Error Message 상수화

--- src/constants/errorMessage.ts
+++ src/constants/errorMessage.ts
@@ -4,4 +4,6 @@ export const API_ERROR_MESSAGE = {
 
 export const POST_EDIT_ERROR_MESSAGE = {
   URL_PROMPT_ERROR_URL: "유효하지 않은 URL 입니다!!",
+  SUBMIT_ERROR_NEW_POST: "등록에 실패했습니다 다시 시도해주세요.. 🥹",
+  SUBMIT_ERROR_UPDATE_POST: "수정에 실패했습니다 다시 시도해주세요.. 🥹",
 }
