c5a6b517eebf831ff88f575de57bb1746b7256ba
J <68155263+jxxxxe@users.noreply.github.com>
2024-01-08 11:27:08 +0900

    [Refactoring ⚙️] grid column 개수를 auto-fill에서 반응형으로 수정

--- src/pages/Profile/components/UserPosts/components/UserPostList/UserPostList.Styles.ts
+++ src/pages/Profile/components/UserPosts/components/UserPostList/UserPostList.Styles.ts
@@ -1,19 +1,28 @@
 import styled from "styled-components"
 
 export const UserPostListContainer = styled.div`
-  width: 100%;
-
   display: grid;
-  grid-template-columns: repeat(auto-fill, 30rem);
+  grid-template-columns: repeat(4, 30rem);
   gap: 6rem;
   justify-content: center;
 
-  margin-top: 10rem;
-  padding: 0 10rem;
+  padding: 5rem;
+
+  @media (max-width: 750px) {
+    grid-template-columns: repeat(1, 30rem);
+  }
+
+  @media (min-width: 750px) and (max-width: 1100px) {
+    grid-template-columns: repeat(2, 30rem);
+  }
+
+  @media (min-width: 1100px) and (max-width: 1400px) {
+    grid-template-columns: repeat(3, 30rem);
+  }
 `
 
 export const UserPostListItemContainer = styled.div`
-  min-width: 30rem;
+  width: 30rem;
   aspect-ratio: 1;
 
   border-radius: ${({ theme }) => theme.radius.size20};
