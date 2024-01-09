01eed8b4b01878e6c6ec9507099b4f66a7add063
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-10 08:59:45 +0900

    [Feat ✏️] PostDetail Viewer 레이아웃 및 Card 연결

---
+++ src/components/PostDetail/components/PostDetailViewer/PostDetailViewer.Styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const PostDetailViewerLayout = styled.div`
+  width: 46rem;
+  height: 63rem;
+
+  background-color: ${({ theme }) => theme.colors.sub_alt};
+  border-radius: ${({ theme }) => theme.radius.size12};
+`
--- src/components/PostDetail/components/PostDetailViewer/PostDetailViewer.tsx
+++ src/components/PostDetail/components/PostDetailViewer/PostDetailViewer.tsx
@@ -1,5 +1,23 @@
-const PostDetailViewer = () => {
-  return <></>
+import PostCard from "@/components/PostCard/PostCard"
+import * as S from "./PostDetailViewer.Styles"
+import { Post } from "@/types"
+
+interface PostDetailViewerProps {
+  post: Post
+}
+
+const PostDetailViewer = ({ post }: PostDetailViewerProps) => {
+  return (
+    <S.PostDetailViewerLayout>
+      <PostCard
+        authUserProfile=""
+        isNoneProfile={true}
+        thumbnail=""
+        content=""
+        mediaUrl=""
+      />
+    </S.PostDetailViewerLayout>
+  )
 }
 
 export default PostDetailViewer
