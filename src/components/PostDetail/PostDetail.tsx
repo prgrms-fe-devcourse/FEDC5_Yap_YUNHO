f82cf40850debdf9cd88c0e49d4f65ff79a09c92
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-10 03:01:01 +0900

    [Feat ✏️] fetch Post 구현

--- src/components/PostDetail/PostDetail.tsx
+++ src/components/PostDetail/PostDetail.tsx
@@ -2,6 +2,12 @@ import * as GS from "@/components/Modal/ModalGlobal.Styles"
 import Modal from "../Modal/Modal"
 import PostDetailViewer from "./components/PostDetailViewer/PostDetailViewer"
 import PostDetailInfo from "./components/PostDetailInfo/PostDetailInfo"
+import { useParams } from "react-router-dom"
+import { useQuery } from "@tanstack/react-query"
+import { API } from "@/apis/Api"
+import { JSONPost, Post } from "@/types"
+
+const POST_DETAIL_QUERY_KEY = "POST_DETAIL_QUERY_KEY"
 
 interface PostDetailProps {
   isShow: boolean
@@ -9,6 +15,30 @@ interface PostDetailProps {
 }
 
 const PostDetail = ({ onClose, isShow }: PostDetailProps) => {
+  const { id } = useParams()
+
+  const { data } = useQuery({
+    queryKey: [POST_DETAIL_QUERY_KEY, id],
+    queryFn: async () => {
+      return await API.get(`/posts/${id}`).then((res) => res.data)
+    },
+    select: (fetchPost: JSONPost) => {
+      const { content, mediaUrl, thumbnail } = JSON.parse(fetchPost.title)
+
+      const detailPost: Post = {
+        ...fetchPost,
+        title: {
+          mediaUrl: mediaUrl,
+          thumbnail: thumbnail,
+          content: content,
+        },
+      }
+      return detailPost
+    },
+  })
+
+  console.log(data)
+
   return (
     <Modal
       isShow={isShow}
