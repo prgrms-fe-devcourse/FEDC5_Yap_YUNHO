f632411b6f864f20b64219109f84f2bc1b0b71b5
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-09 10:52:53 +0900

    [Remove 🗑️] vercel 오류 개선 1

---
+++ src/components/PostCard/components/MediaPlayer/constants/MediaPlayer.ts
@@ -0,0 +1,9 @@
+export const MEDIA_PLAYER_PROGRESS_BAR_RANGE_PERCENTAGE = {
+  MIN: 0,
+  MAX: 1,
+}
+
+export const MEDIA_PLAYER_EMPTY_URL_KEYWORD = "this is other Page"
+
+// 0 ~ 1 사이의 값 허용
+export const MEDIA_PLAYER_INITIAL_VOLUME_PERCENTAGE = 0.1
