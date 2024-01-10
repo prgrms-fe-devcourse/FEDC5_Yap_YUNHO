3fbe6b8c671e6dc4457ee753ab485fb855269af2
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-10 19:44:17 +0900

    [Feat ✏️] Status 버튼 외형 작업

--- src/components/PostDetail/components/PostDetailInfo/components/PostDetailStatus/PostDetailStatus.Styles.ts
+++ src/components/PostDetail/components/PostDetailInfo/components/PostDetailStatus/PostDetailStatus.Styles.ts
@@ -7,6 +7,8 @@ export const PostDetailStatus = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  user-select: none;
 `
 
 export const PostDetailStatusActions = styled.div`
@@ -14,9 +16,18 @@ export const PostDetailStatusActions = styled.div`
 
   display: flex;
   align-items: center;
+
+  gap: 3rem;
+
+  svg {
+    font-size: 3.2rem;
+    color: ${({ theme }) => theme.colors.white};
+
+    cursor: pointer;
+  }
 `
 
-export const PostDetailLike = styled.button`
+export const PostDetailLike = styled.button<{ $isMyLikePost: boolean }>`
   font-size: ${({ theme }) => theme.fontSizes.semiLarge};
   color: ${({ theme }) => theme.colors.white};
 
@@ -25,6 +36,16 @@ export const PostDetailLike = styled.button`
   gap: 0.4rem;
 
   svg {
+    margin-right: 1rem;
+
     font-size: 2.4rem;
+
+    transition: transform ${({ theme }) => theme.transition.normal};
+    color: ${({ theme, $isMyLikePost }) =>
+      $isMyLikePost ? theme.colors.point : theme.colors.white};
+
+    &:hover {
+      transform: rotate(-15deg) scale(1.3) translate(-0.4rem, -0.4rem);
+    }
   }
 `
--- src/components/PostDetail/components/PostDetailInfo/components/PostDetailStatus/PostDetailStatus.tsx
+++ src/components/PostDetail/components/PostDetailInfo/components/PostDetailStatus/PostDetailStatus.tsx
@@ -1,8 +1,10 @@
 import { Post, User } from "@/types"
 import * as S from "./PostDetailStatus.Styles"
-import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt"
 import { convertFollowCount } from "@/util/convertFollowCount"
 import PostDetailEditActions from "./components/PostDetailEditActions"
+import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt"
+import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt"
+import LinkIcon from "@mui/icons-material/Link"
 
 interface PostDetailStatusProps {
   post: Post
@@ -17,23 +19,28 @@ const PostDetailStatus = ({
   onClose,
 }: PostDetailStatusProps) => {
   const { likes } = post
+  console.log(post)
+
+  const isMyLikePost = false
 
   console.log(authUser, isMyPost)
   return (
     <S.PostDetailStatus>
       <S.PostDetailStatusActions>
-        <S.PostDetailLike>
-          <ThumbUpOffAltIcon />
+        <S.PostDetailLike $isMyLikePost={isMyLikePost}>
+          {isMyLikePost ? <ThumbUpAltIcon /> : <ThumbUpOffAltIcon />}
+
           {convertFollowCount(likes.length)}
         </S.PostDetailLike>
+        <LinkIcon />
       </S.PostDetailStatusActions>
 
-      {
+      {isMyPost && (
         <PostDetailEditActions
           onClose={onClose}
           post={post}
         />
-      }
+      )}
     </S.PostDetailStatus>
   )
 }
