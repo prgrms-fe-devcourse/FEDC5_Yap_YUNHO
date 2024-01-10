96de8bcb59082955177e442a9d6ad3babcbd9766
J <68155263+jxxxxe@users.noreply.github.com>
2024-01-10 13:56:24 +0900

    [Feat ✏️] search modal input 구현

--- src/components/Search/components/SearchInput/components/SearchInputBar/SearchInputBar.Styles.ts
+++ src/components/Search/components/SearchInput/components/SearchInputBar/SearchInputBar.Styles.ts
@@ -1,6 +1,44 @@
 import styled from "styled-components"
 
-export const SearchInputBarLayout = styled.div`
+export const SearchBarForm = styled.form`
+  display: flex;
   width: 100%;
-  background-color: ${({ theme }) => theme.colors.black};
+  padding: 1.4rem;
+
+  border-radius: 1rem;
+  border: ${({ theme }) => `0.1rem solid ${theme.colors.sub}`};
+
+  background-color: ${({ theme }) => theme.colors.white};
+`
+
+export const SearchBarInput = styled.input`
+  width: 100%;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.gray40};
+  }
+
+  font-size: ${({ theme }) => theme.fontSizes.large};
+
+  /* (다음 Pr에서 주석 삭제 예정)
+   검색어를 입력하면 검색바 오른쪽 끝에 나오는 x자 버튼입니다.
+   위 : IE, 
+   아래 : Chrome
+  */
+  &::-ms-clear,
+  &::-ms-reveal {
+    cursor: pointer;
+  }
+
+  &::-webkit-search-decoration,
+  &::-webkit-search-cancel-button,
+  &::-webkit-search-results-button,
+  &::-webkit-search-results-decoration {
+    cursor: pointer;
+  }
+`
+export const SearchIconLayout = styled.div`
+  svg {
+    font-size: 3rem;
+  }
 `
--- src/components/Search/components/SearchInput/components/SearchInputBar/SearchInputBar.tsx
+++ src/components/Search/components/SearchInput/components/SearchInputBar/SearchInputBar.tsx
@@ -1,7 +1,37 @@
+import { API } from "@/apis/Api"
 import * as S from "./SearchInputBar.Styles"
+import { useState } from "react"
+import { Search } from "@mui/icons-material"
 
 const SearchInputBar = () => {
-  return <S.SearchInputBarLayout>SearchInputBar</S.SearchInputBarLayout>
+  const [input, setInput] = useState("")
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+
+    // filter 컴포넌트, result 컴포넌트 작업 후 수정 예정
+    const { data } = await API.get(`/search/all/${input}`)
+    console.log(data)
+  }
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value)
+  }
+
+  return (
+    <S.SearchBarForm onSubmit={handleSubmit}>
+      <S.SearchBarInput
+        type="search"
+        placeholder="사용자나 게시물을 검색해보세요"
+        name="searchbar"
+        onChange={handleInputChange}
+      />
+      {!input && (
+        <S.SearchIconLayout>
+          <Search />
+        </S.SearchIconLayout>
+      )}
+    </S.SearchBarForm>
+  )
 }
 
 export default SearchInputBar
