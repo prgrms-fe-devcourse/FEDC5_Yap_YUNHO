415a02489ec200cdb2373625ec0b2b2b72ce6117
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-11 00:12:37 +0900

    [Feat ✏️] Input 제작

--- src/components/PostDetail/components/PostDetailInfo/components/PostDetailComment/components/PostCommentInput/PostCommentInput.tsx
+++ src/components/PostDetail/components/PostDetailInfo/components/PostDetailComment/components/PostCommentInput/PostCommentInput.tsx
@@ -1,7 +1,18 @@
 import * as S from "./PostCommentInput.Styles"
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward"
 
 const PostCommentInput = () => {
-  return <S.PostCommentInputLayout></S.PostCommentInputLayout>
+  return (
+    <S.PostCommentInputLayout>
+      <S.PostCommentAuthUserProfile $authUserImage={""} />
+      <S.PostCommentInputContainer>
+        <S.PostCommentInput type="text" />
+        <S.PostCommentButton>
+          <ArrowUpwardIcon />
+        </S.PostCommentButton>
+      </S.PostCommentInputContainer>
+    </S.PostCommentInputLayout>
+  )
 }
 
 export default PostCommentInput
