2ba2b41c01a8f902cca876dc009c6bd064752de9
최훈오 <gnsdh86166@gmail.com>
2024-01-10 21:08:58 +0900

    [Refactoring ⚙️] 모달 경로 수정

--- src/components/PostEdit/PostEdit.tsx
+++ src/components/PostEdit/PostEdit.tsx
@@ -9,7 +9,7 @@ import { useParams } from "react-router-dom"
 import { API } from "@/apis/Api"
 import { JSONPost, PostContent } from "@/types"
 import PostEditAuthChecker from "./components/PostEditAuthChecker"
-import Modal from "./../Modal/Modal"
+import Modal from "../Modal/Modal"
 
 interface PostEditProps {
   onClose: () => void
