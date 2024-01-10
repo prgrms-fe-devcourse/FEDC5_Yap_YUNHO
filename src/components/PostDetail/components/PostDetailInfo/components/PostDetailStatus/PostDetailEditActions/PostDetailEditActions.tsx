7158e110b1efc302b7de5711e0f98a077b5b98a0
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-10 18:48:31 +0900

    [Feat ✏️] Edit Actions 구현

---
+++ src/components/PostDetail/components/PostDetailInfo/components/PostDetailStatus/PostDetailEditActions/PostDetailEditActions.tsx
@@ -0,0 +1,12 @@
+import * as S from "./PostDetailEditActions.Styles"
+
+const PostDetailEditActions = () => {
+  return (
+    <S.PostDetailEditActions>
+      <S.PostDetailEditButton>{"수정"}</S.PostDetailEditButton>
+      <S.PostDetailEditButton>{"삭제"}</S.PostDetailEditButton>
+    </S.PostDetailEditActions>
+  )
+}
+
+export default PostDetailEditActions
