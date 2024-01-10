0337cbb131d623015741205d282d9fa34b2355e8
최훈오 <gnsdh86166@gmail.com>
2024-01-10 19:46:09 +0900

    [Remove 🗑️] 로그인 페이지 Navbar왼쪽만 보이게 하기

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,15 +1,10 @@
 import LoginComponent from "./components/Login/LoginComponent"
 import ServiceInfoComponent from "./components/ServiceInfo/ServiceInfoComponent"
 import * as S from "./Login.styles"
-import { useNavigate } from "react-router-dom"
 
 export default function Login() {
-  const navigate = useNavigate()
   return (
     <S.LoginLayout>
-      <S.ServiceLogoContainer>
-        <S.ServiceLogo onClick={() => navigate("/")} />
-      </S.ServiceLogoContainer>
       <S.LoginFormContainer>
         <ServiceInfoComponent />
         <LoginComponent />
