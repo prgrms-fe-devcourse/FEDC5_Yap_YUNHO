76e64f47515b7fa80bf4bc4d1d4beeb9a0bfb21b
최훈오 <gnsdh86166@gmail.com>
2024-01-10 13:11:55 +0900

    [Feat ✏️] 채팅이 추가될때마다 아래로 자동 스크롤 기능 추가

--- src/pages/DirectMessage/components/DMChattingList/DMChattingList.tsx
+++ src/pages/DirectMessage/components/DMChattingList/DMChattingList.tsx
@@ -4,17 +4,29 @@ import DMMessageItem from "./DMMessageItem"
 import { Message } from "@/types"
 import DMInput from "./DMInput"
 import useChattingList from "../../hooks/useChattingList"
+import { useEffect, useRef } from "react"
 
 const DMChattingList = () => {
   const { id } = useParams()
 
   const { data: MessageList } = useChattingList(id || "")
+  const scrollRef = useRef<HTMLDivElement>(null)
+
+  const scrollToBottom = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight
+    }
+  }
+
+  useEffect(() => {
+    scrollToBottom()
+  })
 
   return (
     <S.DMChattingListLayout>
       {id && (
         <>
-          <S.DMMessageList>
+          <S.DMMessageList ref={scrollRef}>
             {MessageList?.map((list: Message) => (
               <DMMessageItem
                 key={list.createdAt}
