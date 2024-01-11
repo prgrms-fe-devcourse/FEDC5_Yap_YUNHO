d2ac5b818e94130c7a5170b3162fecf7f9589191
J <68155263+jxxxxe@users.noreply.github.com>
2024-01-12 00:25:57 +0900

    [Design 🎨] 결과 아이템 Height 변경

--- src/components/Search/components/SearchResult/components/SearchResultItem/SearchResultItem.Styles.ts
+++ src/components/Search/components/SearchResult/components/SearchResultItem/SearchResultItem.Styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components"
 export const SearchResultItemContainer = styled.div`
   display: flex;
   align-items: center;
-  height: 7rem;
+  height: 6em;
 
   padding: 1rem 3rem 1rem 0;
   gap: 1rem;
