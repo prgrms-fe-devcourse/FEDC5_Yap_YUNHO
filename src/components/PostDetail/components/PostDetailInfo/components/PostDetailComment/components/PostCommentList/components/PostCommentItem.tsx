a0515ff56d42b998efc6b30b28ed722312253aa1
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-11 12:07:33 +0900

    [Feat ✏️] 삭제 준비를 위한 Modal 제작

--- src/components/PostDetail/components/PostDetailInfo/components/PostDetailComment/components/PostCommentList/components/PostCommentItem.tsx
+++ src/components/PostDetail/components/PostDetailInfo/components/PostDetailComment/components/PostCommentList/components/PostCommentItem.tsx
@@ -6,12 +6,20 @@ import ModeEditOutlineIcon from "@mui/icons-material/ModeEditOutline"
 import AccessTimeIcon from "@mui/icons-material/AccessTime"
 import { format, register } from "timeago.js"
 import kolocale from "timeago.js/lib/lang/ko"
+import ConfirmModal from "@/components/Modal/components/ConfirmModal/ConfirmModal"
+import useModal from "@/components/Modal/hooks/useModal"
 
 interface PostCommentItemProps {
   comment: PostComment
+  onDelete: (commentId: string) => void
 }
 
-const PostCommentItem = ({ comment }: PostCommentItemProps) => {
+const PostCommentItem = ({ comment, onDelete }: PostCommentItemProps) => {
+  const {
+    isShowModal: isShowConfirm,
+    showModal: showConfirm,
+    closeModal: closeConfirm,
+  } = useModal()
   const { user } = useAuthUserStore()
   register("ko", kolocale)
 
@@ -21,31 +29,47 @@ const PostCommentItem = ({ comment }: PostCommentItemProps) => {
   const isEditComment = createdAt !== updatedAt
   const convertedData = format(createdAt, "ko")
 
+  const handleDeleteComment = (isAccept: boolean) => {
+    if (!isAccept) {
+      closeConfirm()
+      return
+    }
+    closeConfirm()
+    onDelete(comment._id)
+  }
   return (
-    <S.PostCommentItemLayout>
-      <S.PostCommentItemUserProfile $profile={author.image} />
-      <S.PostCommentItemContainer>
-        <S.PostCommentItemInfo>
-          {`${author.fullName}`}
-          <AccessTimeIcon />
-          {`${convertedData}`}
-          {!isEditComment && <strong>편집됨</strong>}
-        </S.PostCommentItemInfo>
-        <S.PostCommentItemComment>{comment.comment}</S.PostCommentItemComment>
-      </S.PostCommentItemContainer>
-      <S.PostCommentItemActions>
-        {isMyComment && (
-          <>
-            <S.PostCommentItemButton>
-              <DeleteIcon />
-            </S.PostCommentItemButton>
-            <S.PostCommentItemButton>
-              <ModeEditOutlineIcon />
-            </S.PostCommentItemButton>
-          </>
-        )}
-      </S.PostCommentItemActions>
-    </S.PostCommentItemLayout>
+    <>
+      <S.PostCommentItemLayout>
+        <S.PostCommentItemUserProfile $profile={author.image} />
+        <S.PostCommentItemContainer>
+          <S.PostCommentItemInfo>
+            {`${author.fullName}`}
+            <AccessTimeIcon />
+            {`${convertedData}`}
+            {!isEditComment && <strong>편집됨</strong>}
+          </S.PostCommentItemInfo>
+          <S.PostCommentItemComment>{comment.comment}</S.PostCommentItemComment>
+        </S.PostCommentItemContainer>
+        <S.PostCommentItemActions>
+          {isMyComment && (
+            <>
+              <S.PostCommentItemButton onClick={showConfirm}>
+                <DeleteIcon />
+              </S.PostCommentItemButton>
+              <S.PostCommentItemButton>
+                <ModeEditOutlineIcon />
+              </S.PostCommentItemButton>
+            </>
+          )}
+        </S.PostCommentItemActions>
+      </S.PostCommentItemLayout>
+
+      <ConfirmModal
+        isShow={isShowConfirm}
+        message={"정말 삭제하시겠어요?"}
+        onClose={handleDeleteComment}
+      />
+    </>
   )
 }
 
