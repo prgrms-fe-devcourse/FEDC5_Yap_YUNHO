d4377269867311117df89d1f3d0e998bd4606fd8
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-11 10:52:16 +0900

    [Feat ✏️] User Action button 구현

--- src/components/PostDetail/components/PostDetailInfo/components/PostDetailComment/components/PostCommentList/components/PostCommentItem.Styles.ts
+++ src/components/PostDetail/components/PostDetailInfo/components/PostDetailComment/components/PostCommentList/components/PostCommentItem.Styles.ts
@@ -4,7 +4,10 @@ export const PostCommentItemLayout = styled.div`
   width: 100%;
   min-height: 6rem;
 
-  border: 1px solid red;
+  display: flex;
+  gap: 1rem;
+
+  overflow-y: auto;
 `
 
 export const PostCommentItemUserProfile = styled.div<{ $profile: string }>`
@@ -14,5 +17,34 @@ export const PostCommentItemUserProfile = styled.div<{ $profile: string }>`
   border-radius: ${({ theme }) => theme.radius.circle};
   background-image: url(${({ $profile }) => $profile});
   background-color: ${({ theme }) => theme.colors.sub_alt};
+  box-shadow: 0 0 4px 2px rgba(0, 0, 0, 0.5);
   background-size: cover;
+`
+
+export const PostCommentItemContainer = styled.div`
+  flex-grow: 1;
+  border: 1px solid red;
+`
+
+export const PostCommentItemActions = styled.div`
+  width: 4rem;
+  height: 6rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 0.4rem;
+`
+
+export const PostCommentItemButton = styled.button`
+  svg {
+    font-size: 2rem;
+
+    color: ${({ theme }) => theme.colors.point};
+
+    transition: opacity ${({ theme }) => theme.transition.normal};
+    &:hover {
+      opacity: ${({ theme }) => theme.opacity.normal};
+    }
+  }
 `
--- src/components/PostDetail/components/PostDetailInfo/components/PostDetailComment/components/PostCommentList/components/PostCommentItem.tsx
+++ src/components/PostDetail/components/PostDetailInfo/components/PostDetailComment/components/PostCommentList/components/PostCommentItem.tsx
@@ -1,14 +1,36 @@
 import { PostComment } from "@/types"
 import * as S from "./PostCommentItem.Styles"
+import useAuthUserStore from "@/stores/useAuthUserStore"
+import DeleteIcon from "@mui/icons-material/Delete"
+import ModeEditOutlineIcon from "@mui/icons-material/ModeEditOutline"
 
 interface PostCommentItemProps {
   comment: PostComment
 }
 
 const PostCommentItem = ({ comment }: PostCommentItemProps) => {
+  const { user } = useAuthUserStore()
+  const { author } = comment
+
+  const isMyComment = author._id === user._id
+
+  console.log(isMyComment)
   return (
     <S.PostCommentItemLayout>
-      <S.PostCommentItemUserProfile $profile={comment.author.image} />
+      <S.PostCommentItemUserProfile $profile={author.image} />
+      <S.PostCommentItemContainer></S.PostCommentItemContainer>
+      <S.PostCommentItemActions>
+        {isMyComment && (
+          <>
+            <S.PostCommentItemButton>
+              <DeleteIcon />
+            </S.PostCommentItemButton>
+            <S.PostCommentItemButton>
+              <ModeEditOutlineIcon />
+            </S.PostCommentItemButton>
+          </>
+        )}
+      </S.PostCommentItemActions>
     </S.PostCommentItemLayout>
   )
 }
