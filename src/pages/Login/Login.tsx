1c4d46c32f2c123f224c0c1011ecfb56484e34ee
dlsxjzld <sinjuk12@gmail.com>
2024-01-12 19:46:02 +0900

    [Feat ✏️] 로그인한 회원은 회원가입 페이지에 접근 불가 구현

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -10,7 +10,7 @@ export default function Login() {
   return (
     <>
       {isLoggedIn ? (
-        <Navigate to={"/"}></Navigate>
+        <Navigate to="/" />
       ) : (
         <S.LoginLayout>
           <S.LoginComponentContainer>
--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -2,8 +2,12 @@ import { useState } from "react"
 import * as S from "./Signup.Styles"
 import FirstSignupForm from "./components/FirstSignupForm/FirstSignupForm"
 import SecondSignupForm from "./components/SecondSignupForm/SecondSignupForm"
+import { Navigate } from "react-router-dom"
+import useAuthUserStore from "@/stores/useAuthUserStore"
 
 export default function Signup() {
+  const { isLoggedIn } = useAuthUserStore()
+
   const [selectedFormComponent, setSelectedFormComponent] = useState("First")
 
   const handleSelectedFormComponent = (): void => {
@@ -11,14 +15,20 @@ export default function Signup() {
   }
 
   return (
-    <S.SignupLayout>
-      {selectedFormComponent === "First" ? (
-        <FirstSignupForm
-          handleSelectedFormComponent={handleSelectedFormComponent}
-        />
+    <>
+      {isLoggedIn ? (
+        <Navigate to="/" />
       ) : (
-        <SecondSignupForm />
+        <S.SignupLayout>
+          {selectedFormComponent === "First" ? (
+            <FirstSignupForm
+              handleSelectedFormComponent={handleSelectedFormComponent}
+            />
+          ) : (
+            <SecondSignupForm />
+          )}
+        </S.SignupLayout>
       )}
-    </S.SignupLayout>
+    </>
   )
 }
