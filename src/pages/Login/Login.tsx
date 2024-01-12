7f3e9b7d47841c88127dfc4daab7993b2e4dc624
dlsxjzld <sinjuk12@gmail.com>
2024-01-12 19:41:04 +0900

    [Feat ✏️] 로그인한 회원은 로그인 페이지에 접근 불가 구현

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,14 +1,24 @@
+import useAuthUserStore from "@/stores/useAuthUserStore"
 import LoginComponent from "./components/Login/LoginComponent"
 import ServiceInfoComponent from "./components/ServiceInfo/ServiceInfoComponent"
 import * as S from "./Login.styles"
+import { Navigate } from "react-router-dom"
 
 export default function Login() {
+  const { isLoggedIn } = useAuthUserStore()
+
   return (
-    <S.LoginLayout>
-      <S.LoginComponentContainer>
-        <ServiceInfoComponent />
-        <LoginComponent />
-      </S.LoginComponentContainer>
-    </S.LoginLayout>
+    <>
+      {isLoggedIn ? (
+        <Navigate to={"/"}></Navigate>
+      ) : (
+        <S.LoginLayout>
+          <S.LoginComponentContainer>
+            <ServiceInfoComponent />
+            <LoginComponent />
+          </S.LoginComponentContainer>
+        </S.LoginLayout>
+      )}
+    </>
   )
 }
