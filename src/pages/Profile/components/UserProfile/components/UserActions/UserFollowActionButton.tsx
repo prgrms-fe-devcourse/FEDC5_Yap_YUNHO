f6977ed500b3544a4348afa3a6e184c968843813
J <68155263+jxxxxe@users.noreply.github.com>
2024-01-15 08:12:18 +0900

    [Feat ✏️] 팔로우 버튼 기능 구현

---
+++ src/pages/Profile/components/UserProfile/components/UserActions/UserFollowActionButton.tsx
@@ -0,0 +1,69 @@
+import * as S from "./UserActions.Styles"
+
+import useFetchFollow from "@/hooks/useFetchFollow"
+import useFetchUnFollow from "@/hooks/useFetchUnFollow"
+import { useEffect, useState } from "react"
+import { API } from "@/apis/Api"
+import { useParams } from "react-router-dom"
+import useAuthUserStore from "@/stores/useAuthUserStore"
+
+interface followInfoProp {
+  user: string
+}
+
+const UserFollowActionButton = () => {
+  const { id } = useParams()
+  const { myId } = useAuthUserStore()
+  const { fetchFollowMutate } = useFetchFollow()
+  const { fetchUnFollowMutate } = useFetchUnFollow()
+
+  const [followInfoId, setFollowInfoId] = useState("")
+
+  useEffect(() => {
+    API.get(`/users/${myId}`)
+      .then((res) => res.data)
+      .then(({ following }) => {
+        const followInfo = following.find(
+          ({ user }: followInfoProp) => user === id,
+        )
+
+        if (followInfo) {
+          setFollowInfoId(followInfo._id)
+        }
+      })
+  }, [id, myId])
+
+  const handleFollowButton = () => {
+    if (fetchFollowMutate.isPending || fetchUnFollowMutate.isPending) {
+      return
+    }
+
+    if (followInfoId) {
+      fetchUnFollowMutate.mutate(followInfoId, {
+        onSuccess: () => {
+          setFollowInfoId("")
+        },
+      })
+
+      return
+    }
+
+    fetchFollowMutate.mutate(id!, {
+      onSuccess: ({ _id }) => {
+        setFollowInfoId(_id)
+      },
+    })
+  }
+
+  return (
+    <S.UserActionButton
+      $width={9}
+      onClick={handleFollowButton}
+      $isFollowing={!!followInfoId}
+    >
+      {followInfoId ? "언팔로우" : "팔로우"}
+    </S.UserActionButton>
+  )
+}
+
+export default UserFollowActionButton
