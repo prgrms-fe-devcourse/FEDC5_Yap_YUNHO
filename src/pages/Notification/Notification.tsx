b2382de3180a543b15bc4dbcd418f0ff1f25c47c
최훈오 <gnsdh86166@gmail.com>
2024-01-14 14:45:44 +0900

    [Feat ✏️] 알림 길이 더미데이터

--- src/pages/Notification/Notification.tsx
+++ src/pages/Notification/Notification.tsx
@@ -1,12 +1,63 @@
 import * as S from "./Notification.Styles"
 import NotificationTitle from "./NotificationTitle"
 import NotificationList from "./NotificationList"
-
+const DUMMY_DATA = [
+  {
+    seen: false,
+    _id: "1",
+    createdAt: "2024-01-12T16:49:28.581Z",
+    updatedAt: "2024-01-12T16:49:28.581Z",
+    author: {
+      coverImage: String, // 커버 이미지(사용되지 않음)
+      image: String,
+      role: String,
+      emailVerified: Boolean, // 사용되지 않음
+      banned: Boolean, // 사용되지 않음
+      isOnline: Boolean,
+      posts: [],
+      likes: [],
+      comments: [],
+      followers: [],
+      following: [],
+      notifications: [],
+      messages: [],
+      _id: "123124214",
+      fullName: "이재영",
+      email: "leey153@naver.com",
+      createdAt: "2021-10-15T20:48:19.816Z",
+      updatedAt: "2021-10-15T20:48:19.816Z",
+    },
+    user: {
+      coverImage: String, // 커버 이미지(사용되지 않음)
+      image: String,
+      role: String,
+      emailVerified: Boolean, // 사용되지 않음
+      banned: Boolean, // 사용되지 않음
+      isOnline: Boolean,
+      posts: [],
+      likes: [],
+      comments: [],
+      followers: [],
+      following: [],
+      notifications: [],
+      messages: [],
+      _id: "123124214",
+      fullName: "이재영",
+      email: "leey153@naver.com",
+      createdAt: "2021-10-15T20:48:19.816Z",
+      updatedAt: "2021-10-15T20:48:19.816Z",
+    },
+    message: "", // 메시지 아이디
+    // post: "",
+    // comment: "",
+    // follow: "",
+  },
+]
 const Notification = () => {
   return (
     <S.NotificationLayout>
-      <NotificationTitle />
-      <NotificationList />
+      <NotificationTitle DUMMY_DATA={DUMMY_DATA} />
+      <NotificationList DUMMY_DATA={DUMMY_DATA} />
     </S.NotificationLayout>
   )
 }
--- src/pages/Notification/NotificationTitle.tsx
+++ src/pages/Notification/NotificationTitle.tsx
@@ -1,11 +1,26 @@
 import * as S from "./Notification.Styles"
+import { Notification } from "./../../types/index"
 
-const NotificationTitle = () => {
+interface NotificationTitleProps {
+  DUMMY_DATA: Notification[]
+}
+
+const NotificationTitle = ({ DUMMY_DATA }: NotificationTitleProps) => {
+  const { seen, _id, createdAt, author, user, message } = DUMMY_DATA
+  if (!DUMMY_DATA) {
+    return
+  }
+  const NotificationLength = DUMMY_DATA.filter((notification: Notification) => {
+    return !notification.seen
+  }).length
+
   return (
     <S.NotificationTitleLayout>
       <S.NotificationTitleContainer>
         <S.NotificationTitleList>알림 목록</S.NotificationTitleList>
-        <S.NotificationTitleListNumber>4</S.NotificationTitleListNumber>
+        <S.NotificationTitleListNumber>
+          {NotificationLength}
+        </S.NotificationTitleListNumber>
       </S.NotificationTitleContainer>
       <S.NotificationDeleteButton>알림 지우기</S.NotificationDeleteButton>
     </S.NotificationTitleLayout>
