3112fc67884c81a13833c035d1109080b0ec7ec8
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-15 19:05:24 +0900

    [Feat ✏️] Like 알림 추가

--- src/components/PostDetail/hooks/useLikePost.tsx
+++ src/components/PostDetail/hooks/useLikePost.tsx
@@ -3,6 +3,8 @@ import AlertModal from "@/components/Modal/components/AlertModal/AlertModal"
 import useModal from "@/components/Modal/hooks/useModal"
 import { POST_DETAIL_ERROR_MESSAGE } from "@/constants/errorMessage"
 import { AUTH_API } from "@/apis/Api"
+import { Like } from "@/types"
+import sendNotification from "@/apis/sendNotification"
 
 const MUTATION_KEY_LIKE_POST_KEY = "IT_IS_LIKE_MUTATION_KEY_546786723746238"
 
@@ -13,7 +15,13 @@ const useLikePost = () => {
   const fetchLikeMutate = useMutation({
     mutationKey: [MUTATION_KEY_LIKE_POST_KEY],
     mutationFn: fetchLikePost,
-    onSuccess: () => {
+    onSuccess: (response: Like) => {
+      sendNotification({
+        notificationType: "LIKE",
+        notificationTypeId: response._id,
+        postId: response.post,
+        userId: response.user,
+      })
       queryClient.refetchQueries()
     },
     onError: () => {
