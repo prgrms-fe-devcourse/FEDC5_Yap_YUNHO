eed27b9107925ad5e6233cbad1dfab0c3475f38c
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-16 00:54:41 +0900

    [Feat ✏️] PostEdit에 대한 Detail 예외처리

--- src/components/PostDetail/components/PostDetailViewer/PostDetailViewer.tsx
+++ src/components/PostDetail/components/PostDetailViewer/PostDetailViewer.tsx
@@ -1,12 +1,14 @@
 import PostCard from "@/components/PostCard/PostCard"
 import * as GS from "@/components/Modal/ModalGlobal.Styles"
 import { Post } from "@/types"
+import usePostEditModalStore from "@/components/PostEdit/stores/usePostEditModalStore"
 
 interface PostDetailViewerProps {
   post: Post
 }
 
 const PostDetailViewer = ({ post }: PostDetailViewerProps) => {
+  const { isShowEditModal } = usePostEditModalStore()
   const { mediaUrl, content, thumbnail } = post.title
   return (
     <GS.PostModalGlobalViewerLayout>
@@ -16,6 +18,7 @@ const PostDetailViewer = ({ post }: PostDetailViewerProps) => {
         content={content}
         mediaUrl={mediaUrl}
         postId={post._id}
+        isBlock={isShowEditModal}
       />
     </GS.PostModalGlobalViewerLayout>
   )
