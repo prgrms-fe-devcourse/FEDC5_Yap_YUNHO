2b28c6747fb98b34c584ca08961f6636255385e6
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-15 18:12:55 +0900

    [Feat ✏️] action 렌더링 조건 처리

--- src/components/UserInfoPopover/Components/UserInfoActions/UserInfoActions.tsx
+++ src/components/UserInfoPopover/Components/UserInfoActions/UserInfoActions.tsx
@@ -1,20 +1,30 @@
 import { User } from "@/types"
 import * as S from "./UserInfoActions.Styles"
 import { useNavigate } from "react-router-dom"
+import useAuthUserStore from "@/stores/useAuthUserStore"
 
 interface UserInfoActionsProps {
   user: User
 }
 
 const UserInfoActions = ({ user }: UserInfoActionsProps) => {
+  const { user: authUser, isLoggedIn } = useAuthUserStore()
   const navigate = useNavigate()
+
+  const isMyInformation = authUser._id === user._id
 
   return (
     <S.UserInfoActionsLayout>
-      <S.UserInfoButtons onClick={() => navigate(`/directmessage/${user._id}`)}>
-        DM
-      </S.UserInfoButtons>
-      <S.UserInfoButtons>언 팔로우</S.UserInfoButtons>
+      {isLoggedIn && !isMyInformation && (
+        <>
+          <S.UserInfoButtons
+            onClick={() => navigate(`/directmessage/${user._id}`)}
+          >
+            DM
+          </S.UserInfoButtons>
+          <S.UserInfoButtons>언 팔로우</S.UserInfoButtons>
+        </>
+      )}
     </S.UserInfoActionsLayout>
   )
 }
