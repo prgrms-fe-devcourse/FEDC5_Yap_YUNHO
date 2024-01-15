e5d9bd06c9d9f6f3f0445da128146ebc1afbf66b
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-15 18:07:59 +0900

    [Feat ✏️] 프로필 사진 navigate 연결

--- src/components/UserInfoPopover/Components/UserInfoProfile/UserInfoProfile.tsx
+++ src/components/UserInfoPopover/Components/UserInfoProfile/UserInfoProfile.tsx
@@ -1,14 +1,19 @@
 import { User } from "@/types"
 import * as S from "./UserInfoProfile.Styles"
+import { useNavigate } from "react-router-dom"
 
 interface UserInfoProfileProps {
   user: User
 }
 
 const UserInfoProfile = ({ user }: UserInfoProfileProps) => {
+  const navigate = useNavigate()
   return (
     <S.UserInfoProfileLayout>
-      <S.UserInfoProfile $url={user.image} />
+      <S.UserInfoProfile
+        $url={user.image}
+        onClick={() => navigate(`/profile/${user._id}`)}
+      />
       <S.UserInfoNickNameText>{user.fullName}</S.UserInfoNickNameText>
     </S.UserInfoProfileLayout>
   )
