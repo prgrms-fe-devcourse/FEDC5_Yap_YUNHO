e012847c251b069fa9c22867cc8ca0e93a6b3bef
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-15 18:10:35 +0900

    [Feat ✏️] DM 페이지 버튼 navigate 연결

--- src/components/UserInfoPopover/Components/UserInfoActions/UserInfoActions.tsx
+++ src/components/UserInfoPopover/Components/UserInfoActions/UserInfoActions.tsx
@@ -1,16 +1,19 @@
 import { User } from "@/types"
 import * as S from "./UserInfoActions.Styles"
+import { useNavigate } from "react-router-dom"
 
 interface UserInfoActionsProps {
   user: User
 }
 
 const UserInfoActions = ({ user }: UserInfoActionsProps) => {
-  console.log(user)
+  const navigate = useNavigate()
 
   return (
     <S.UserInfoActionsLayout>
-      <S.UserInfoButtons>DM</S.UserInfoButtons>
+      <S.UserInfoButtons onClick={() => navigate(`/directmessage/${user._id}`)}>
+        DM
+      </S.UserInfoButtons>
       <S.UserInfoButtons>언 팔로우</S.UserInfoButtons>
     </S.UserInfoActionsLayout>
   )
