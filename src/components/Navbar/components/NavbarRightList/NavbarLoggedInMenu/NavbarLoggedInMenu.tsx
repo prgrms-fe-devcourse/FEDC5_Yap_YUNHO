799b5d85c948945443af607f7fcf0f6179042934
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-16 01:01:30 +0900

    [Feat ✏️] navbar 게시물 생성 버튼 연결

--- src/components/Navbar/components/NavbarRightList/NavbarLoggedInMenu/NavbarLoggedInMenu.tsx
+++ src/components/Navbar/components/NavbarRightList/NavbarLoggedInMenu/NavbarLoggedInMenu.tsx
@@ -1,24 +1,33 @@
 import { NavbarMenuProps } from "@/components/Navbar/Navbar.Types"
 import { NavbarMenuList } from "./../NavbarRightList.Styles"
 import NavbarMenuItem from "../NavbarMenuItem/NavbarMenuItem"
+import usePostEditModalStore from "@/components/PostEdit/stores/usePostEditModalStore"
+import PostEdit from "@/components/PostEdit/PostEdit"
 
 const NavbarLoggedInMenu = ({
   handleMenuClick,
   $isToggle,
 }: NavbarMenuProps) => {
+  const { isShowEditModal, showEditModal } = usePostEditModalStore()
   return (
-    <NavbarMenuList $isToggle={$isToggle}>
-      <NavbarMenuItem handleMenuClick={handleMenuClick}>
-        {"게시물 생성"}
-      </NavbarMenuItem>
-      <NavbarMenuItem handleMenuClick={handleMenuClick}>{"DM"}</NavbarMenuItem>
-      <NavbarMenuItem handleMenuClick={handleMenuClick}>
-        {"알림"}
-      </NavbarMenuItem>
-      <NavbarMenuItem handleMenuClick={handleMenuClick}>
-        {"로그아웃"}
-      </NavbarMenuItem>
-    </NavbarMenuList>
+    <>
+      <NavbarMenuList $isToggle={$isToggle}>
+        <NavbarMenuItem handleMenuClick={showEditModal}>
+          {"게시물 생성"}
+        </NavbarMenuItem>
+        <NavbarMenuItem handleMenuClick={handleMenuClick}>
+          {"DM"}
+        </NavbarMenuItem>
+        <NavbarMenuItem handleMenuClick={handleMenuClick}>
+          {"알림"}
+        </NavbarMenuItem>
+        <NavbarMenuItem handleMenuClick={handleMenuClick}>
+          {"로그아웃"}
+        </NavbarMenuItem>
+      </NavbarMenuList>
+
+      {isShowEditModal && <PostEdit postId="newPost" />}
+    </>
   )
 }
 
