fc7572bd8c4a6f3421e71c0e4d0eca18b6c4f79f
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-15 18:59:32 +0900

    [Feat ✏️] sendNotification 구현 및 follow 연결

---
+++ src/apis/sendNotification.ts
@@ -0,0 +1,16 @@
+import { AUTH_API } from "./Api"
+
+interface SendNotification {
+  notificationType: "COMMENT" | "FOLLOW" | "LIKE" | "MESSAGE"
+  notificationTypeId: string
+  userId: string
+  postId?: string
+}
+
+const sendNotification = async (body: SendNotification) => {
+  return await AUTH_API.post("/notifications/create", body).then((res) =>
+    console.log(res),
+  )
+}
+
+export default sendNotification
--- src/hooks/useFetchFollow.tsx
+++ src/hooks/useFetchFollow.tsx
@@ -1,8 +1,10 @@
 import { AUTH_API } from "@/apis/Api"
+import sendNotification from "@/apis/sendNotification"
 import AlertModal from "@/components/Modal/components/AlertModal/AlertModal"
 import useModal from "@/components/Modal/hooks/useModal"
 import { POST_DETAIL_ERROR_MESSAGE } from "@/constants/errorMessage"
 import useAuthUserStore from "@/stores/useAuthUserStore"
+import { Follow } from "@/types"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 
 const FETCH_MUTATION_FOLLOW_KEY = "IT_IS_FETCH_MUTATION_KEY_781521784628175281"
@@ -13,8 +15,14 @@ const useFetchFollow = () => {
   const fetchFollowMutate = useMutation({
     mutationKey: [FETCH_MUTATION_FOLLOW_KEY],
     mutationFn: fetchFollow,
-    onSuccess: () => {
+    onSuccess: (response: Follow) => {
       queryClient.refetchQueries()
+
+      sendNotification({
+        notificationType: "FOLLOW",
+        userId: response.user,
+        notificationTypeId: response._id,
+      })
 
       AUTH_API.get("/auth-user")
         .then((res) => res.data)
