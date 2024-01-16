712843f5a114bf48798b505f28936b5daa74f577
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-16 21:20:17 +0900

    [Fix 🪛] API 방어 처리

--- src/components/NotificationModal/hooks/useGetNotification.ts
+++ src/components/NotificationModal/hooks/useGetNotification.ts
@@ -2,16 +2,23 @@ import { useQuery } from "@tanstack/react-query"
 import getNotificationAPI from "./../apis/getNotificationAPI"
 import { Notification } from "@/types/index"
 import useAuthUserStore from "@/stores/useAuthUserStore"
+import usePostDetailModalStore from "@/components/PostDetail/stores/usePostDetailModalStore"
+import usePostEditModalStore from "@/components/PostEdit/stores/usePostEditModalStore"
 
 export const QUERY_KEY_GET_NOTIFICATION = "GET_NOTIFICATION"
 
 const useGetNotification = () => {
   const { isLoggedIn } = useAuthUserStore()
+  const { isShowPostDetail } = usePostDetailModalStore()
+  const { isShowEditModal } = usePostEditModalStore()
+
+  const isNotShowModal = !isShowPostDetail && !isShowEditModal
+
   const { data } = useQuery({
     queryKey: [QUERY_KEY_GET_NOTIFICATION],
     queryFn: getNotificationAPI,
     enabled: isLoggedIn,
-    refetchInterval: 1000 * 5,
+    refetchInterval: isNotShowModal && 1000 * 5,
     gcTime: 1000 * 60 * 5,
     select: (data) => {
       return data.filter(
