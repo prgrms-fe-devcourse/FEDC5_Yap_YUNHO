1ee17eaf3eef4ddc4633e47b28ec422c6ecbf1e4
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-16 11:12:40 +0900

    [Feat ✏️] NotFound Page 시작

---
+++ src/pages/NotFound404/NotFound404.Styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+
+export const NotFound404Layout = styled.section`
+  width: 100%;
+  height: 100%;
+
+  border: 1px solid red;
+`
---
+++ src/pages/NotFound404/NotFound404.tsx
@@ -0,0 +1,7 @@
+import * as S from "./NotFound404.Styles"
+
+const NotFound404 = () => {
+  return <S.NotFound404Layout></S.NotFound404Layout>
+}
+
+export default NotFound404
