01b3d917b3e8884543c9a421be1f2ded2f6676f4
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-17 21:21:51 +0900

    [Change 🚜 ] 멘트

--- src/constants/placeholderMessage.ts
+++ src/constants/placeholderMessage.ts
@@ -1,6 +1,6 @@
 export const POST_EDIT_PLACEHOLDER_MESSAGE = {
   CONTENT_INPUT: "내용을 입력해주세요!",
-  URL_INPUT: "클릭 후, URL을 입력해주세요!",
+  URL_INPUT: "클릭 후, URL을 입력해주세요! ( Youtube 등! )",
   URL_PROMPT: "URL을 입력해주세요 ✏️",
 }
 
