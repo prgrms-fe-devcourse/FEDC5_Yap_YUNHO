508406032ba697b51d58929804614fb1dae04039
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-17 11:41:15 +0900

    [Feat ✏️] Category List 목록 받아오기

---
+++ src/pages/Home/util/getRandomCategory.ts
@@ -0,0 +1,58 @@
+const getRandomCategory = () => {}
+
+export default getRandomCategory
+
+const categoryList = [
+  {
+    _id: "659423dadcb0c5304e3cd5b5",
+    name: "출퇴근길",
+    description: "지루한 출퇴근길, 이 음악들로 잠시나마 여행을 떠나보세요!",
+  },
+  {
+    _id: "6594251ddcb0c5304e3cd5c1",
+    name: "행복한 날",
+    description:
+      "오늘은 기분 좋은 날이신가요? 이 음악들로 행복한 시간을 더욱 즐겨보세요!",
+  },
+  {
+    _id: "6594253edcb0c5304e3cd5c5",
+    name: "에너지 충전",
+    description:
+      "바빠서 지친 하루를 보내셨나요? 이 음악들로 에너지를 충전해보세요!",
+  },
+  {
+    _id: "65942561dcb0c5304e3cd5ca",
+    name: "운동",
+    description:
+      "운동 도중에 듣기 좋은 음악을 찾고 계신가요? 이 음악들로 활력을 더해보세요!",
+  },
+  {
+    _id: "65942573dcb0c5304e3cd5ce",
+    name: "파티",
+    description:
+      "파티를 준비 중이신가요? 이 음악들로 분위기를 한층 더 끌어올려보세요.",
+  },
+  {
+    _id: "659425a2dcb0c5304e3cd5d2",
+    name: "집중",
+    description:
+      "중요한 일에 집중해야 하는 시간이신가요? 이 음악들은 어떤가요?",
+  },
+  {
+    _id: "659425bcdcb0c5304e3cd5d6",
+    name: "휴식",
+    description:
+      "잠시 휴식이 필요하신가요? 이 음악들로 잠시 마음의 여유를 찾아보세요.",
+  },
+  {
+    _id: "659425d5dcb0c5304e3cd5db",
+    name: "슬픔",
+    description: "마음이 아픈 날, 이 음악들이 당신의 슬픔을 달래드릴 거예요.",
+  },
+  {
+    _id: "659425e9dcb0c5304e3cd5e0",
+    name: "잠잘 때",
+    description:
+      "깊은 잠을 청할 수 있는 음악을 찾으신다면? 이 음악들로 달콤한 꿈을 꾸어보세요.",
+  },
+]
