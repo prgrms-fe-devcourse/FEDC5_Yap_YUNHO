2fa614bd537ba55fdb158e1687e62ecf8c73239b
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-28 21:40:50 +0900

    [Refactoring] 리다이렉트 (#208)
    * [Remove 🗑️] 기존 리다이렉트 코드 제거
    
    * [Refactoring ⚙️] App 최상단 리다이렉트 로직 추가
    
    * [Refactoring ⚙️] 상수 관리 변경 및 boolean값 최적화

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
 import YAPRoute from "@/routes"
+import { useEffect } from "react"
+import AlertModal from "./components/Modal/components/AlertModal/AlertModal"
+import useModal from "./components/Modal/hooks/useModal"
+import { NAVBAR_ERROR_MESSAGE } from "./constants/errorMessage"
+import { AUTH_API } from "./apis/Api"
+import authToken from "./stores/authToken"
+import useAuthUserStore from "./stores/useAuthUserStore"
 
 const App = () => {
-  return <YAPRoute />
+  const { showModal, isShowModal, closeModal } = useModal()
+  const { updateUser, setLogout } = useAuthUserStore()
+
+  useEffect(() => {
+    const hasToken = authToken.getToken()
+    if (!hasToken) {
+      return
+    }
+
+    AUTH_API.get("/auth-user")
+      .then((res) => updateUser(res.data))
+      .catch(() => {
+        showModal()
+        setLogout()
+      })
+  }, [setLogout, showModal, updateUser])
+
+  return (
+    <>
+      <YAPRoute />
+
+      <AlertModal
+        isShow={isShowModal}
+        onClose={closeModal}
+        alertMessage={NAVBAR_ERROR_MESSAGE.REDIRECT_AUTH_USER}
+      />
+    </>
+  )
 }
 
 export default App
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,59 +2,34 @@ import { useLocation } from "react-router-dom"
 import * as S from "./Navbar.Styles"
 import NavbarLeftList from "./components/NavbarLeftList/NavbarLeftList"
 import NavbarRightList from "./components/NavbarRightList/NavbarRightList"
-import { useEffect } from "react"
-import authToken from "@/stores/authToken"
-import { AUTH_API } from "@/apis/Api"
-import useAuthUserStore from "@/stores/useAuthUserStore"
-import useModal from "../Modal/hooks/useModal"
-import AlertModal from "../Modal/components/AlertModal/AlertModal"
-import { NAVBAR_ERROR_MESSAGE } from "@/constants/errorMessage"
+import { useMemo } from "react"
 
-const LOGIN_PAGE_PATH_NAME = "/login"
-const SIGNUP_PAGE_PATH_NAME = "/signup"
-const USER_EDIT_PAGE_PATH_NAME = "/useredit"
+const PATH_NAME = {
+  LOGIN_PAGE: "/login",
+  SIGNUP_PAGE: "/signup",
+  USER_EDIT_PAGE: "/useredit",
+}
 
 const Navbar = () => {
-  const { isShowModal, showModal, closeModal } = useModal()
-  const { updateUser, setLogout } = useAuthUserStore()
   const { pathname } = useLocation()
 
-  useEffect(() => {
-    const hasToken = authToken.getToken()
-    if (!hasToken) {
-      return
-    }
-
-    AUTH_API.get("/auth-user")
-      .then((res) => updateUser(res.data))
-      .catch(() => {
-        showModal()
-        setLogout()
-      })
-  }, [updateUser, showModal, setLogout])
+  const isLoginPage = useMemo(
+    () => pathname === PATH_NAME.LOGIN_PAGE,
+    [pathname],
+  )
 
   if (
-    pathname === SIGNUP_PAGE_PATH_NAME ||
-    pathname === USER_EDIT_PAGE_PATH_NAME
+    pathname === PATH_NAME.SIGNUP_PAGE ||
+    pathname === PATH_NAME.USER_EDIT_PAGE
   ) {
     return
   }
 
-  const isLoginPage = pathname === LOGIN_PAGE_PATH_NAME
-
   return (
-    <>
-      <AlertModal
-        isShow={isShowModal}
-        onClose={closeModal}
-        alertMessage={NAVBAR_ERROR_MESSAGE.REDIRECT_AUTH_USER}
-      />
-
-      <S.NavbarLayout>
-        <NavbarLeftList />
-        {!isLoginPage && <NavbarRightList />}
-      </S.NavbarLayout>
-    </>
+    <S.NavbarLayout>
+      <NavbarLeftList />
+      {!isLoginPage && <NavbarRightList />}
+    </S.NavbarLayout>
   )
 }
 
